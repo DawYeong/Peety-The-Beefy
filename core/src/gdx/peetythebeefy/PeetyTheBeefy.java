1ba28844cd4b0124c55cfd1569db70667e719de9
tn200 <tn200@192.168.2.13>
2018-02-26 14:21:55 -0500

    The Beginning (with imports)

--- core/src/gdx/peetythebeefy/PeetyTheBeefy.java
+++ core/src/gdx/peetythebeefy/PeetyTheBeefy.java
@@ -1,33 +1,36 @@
 package gdx.peetythebeefy;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class PeetyTheBeefy extends ApplicationAdapter {
+public class PeetyTheBeefy extends Game {
 	SpriteBatch batch;
-	Texture img;
+        ScrMainMenu scrMainMenu;
+        ScrLvl1 scrLvl1;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+                scrMainMenu = new ScrMainMenu(this);
+                scrLvl1 = new ScrLvl1(this);
+		setScreen(scrMainMenu);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+            super.render();
+        }
+        public void updateScreen(int nScreen) {
+            if(nScreen == 0) {
+                setScreen(scrMainMenu);
+            } else if (nScreen == 1) {
+                setScreen(scrLvl1);
+            }
+            
+        }
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
