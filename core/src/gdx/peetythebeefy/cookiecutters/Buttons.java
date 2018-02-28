3a618a22ce3c42a782666971bfa55b6fd28571ef
TheLegendHimself <bennylam6@gmail.com>
2018-02-28 09:56:06 -0500

    Fixed up Button class

--- core/src/gdx/peetythebeefy/cookiecutters/Buttons.java
+++ core/src/gdx/peetythebeefy/cookiecutters/Buttons.java
@@ -5,10 +5,37 @@
  */
 package gdx.peetythebeefy.cookiecutters;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 /**
  *
  * @author benny
  */
 public class Buttons {
     
+    SpriteBatch batch;
+    Texture img;
+    Sprite sprButton;
+    public float fX, fY, fW, fH;
+    public Buttons(String Image, SpriteBatch batch, float X, float Y, float Width, float Height) {
+        this.batch = batch;
+        img = new Texture(Image);
+        sprButton = new Sprite(img);
+        this.fX = X;
+        this.fY = Y;
+        this.fW = Width;
+        this.fH = Height;
+    }
+    
+    public void Update() {
+        createButtons();
+    }
+    
+    public void createButtons() {
+        batch.begin();
+        batch.draw(sprButton, fX, fY, fW, fH);
+        batch.end();
+    }
 }
