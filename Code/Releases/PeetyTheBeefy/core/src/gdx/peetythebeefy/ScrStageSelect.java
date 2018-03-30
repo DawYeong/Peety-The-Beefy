b9c71213fe999cd260e22f551f0133bbe6c31f51
TheLegendHimself <bennylam6@gmail.com>
2018-03-30 12:33:52 -0400

    button animation

--- Code/Releases/PeetyTheBeefy/core/src/gdx/peetythebeefy/ScrStageSelect.java
+++ Code/Releases/PeetyTheBeefy/core/src/gdx/peetythebeefy/ScrStageSelect.java
@@ -24,12 +24,12 @@ public class ScrStageSelect implements Screen {
     PeetyTheBeefy game;
     SpriteBatch batch;
     Texture texMenuMain, texMenuNew;
-    float fMainX, fNewX;
+    float fMainX, fNewX, fY;
     ArrayList<gdx.peetythebeefy.cookiecutters.Buttons> alButtons = new ArrayList<Buttons>();
 
     public ScrStageSelect(PeetyTheBeefy game) {
         this.game = game;
-        this.batch = game.batch;        
+        this.batch = game.batch;
         texMenuMain = new Texture("mainMenu.png");
         texMenuNew = new Texture("mainMenu2.png");
     }
@@ -38,6 +38,7 @@ public class ScrStageSelect implements Screen {
     public void show() {
         fMainX = 0;
         fNewX = 768;
+        fY = Gdx.graphics.getHeight();
         createButtons();
     }
 
@@ -52,7 +53,18 @@ public class ScrStageSelect implements Screen {
     }
 
     public void createButtons() {
-        alButtons.add(new Buttons("b1", batch, 64, nSCREENHEIGHT - 128, 64, 64));
+        alButtons.add(new Buttons("b1", batch, 64, fY, 64, 64));
+        alButtons.add(new Buttons("b2", batch, 256, fY, 64, 64));
+        alButtons.add(new Buttons("b3", batch, 448, fY, 64, 64));
+        alButtons.add(new Buttons("b4", batch, 640, fY, 64, 64));
+        alButtons.add(new Buttons("b5", batch, 64, fY, 64, 64));
+        alButtons.add(new Buttons("b6", batch, 256, fY, 64, 64));
+        alButtons.add(new Buttons("b7", batch, 448, fY, 64, 64));
+        alButtons.add(new Buttons("b8", batch, 640, fY, 64, 64));
+        alButtons.add(new Buttons("b9", batch, 64, fY, 64, 64));
+        alButtons.add(new Buttons("b10", batch, 256, fY, 64, 64));
+        alButtons.add(new Buttons("b11", batch, 448, fY, 64, 64));
+        alButtons.add(new Buttons("b12", batch, 640, fY, 64, 64));
         alButtons.add(new Buttons("backButton", batch, -8, 0, 96, 32));
     }
 
@@ -64,9 +76,37 @@ public class ScrStageSelect implements Screen {
                 if (i == 0) {
                     System.out.println("moves to lvl1");
                     ScrLvl1.isShowing = false;
+                    //resets the y position of each level button
+                    alButtons.get(0).fY = Gdx.graphics.getHeight();
+                    alButtons.get(1).fY = Gdx.graphics.getHeight();
+                    alButtons.get(2).fY = Gdx.graphics.getHeight();
+                    alButtons.get(3).fY = Gdx.graphics.getHeight();
+                    alButtons.get(4).fY = Gdx.graphics.getHeight();
+                    alButtons.get(5).fY = Gdx.graphics.getHeight();
+                    alButtons.get(6).fY = Gdx.graphics.getHeight();
+                    alButtons.get(7).fY = Gdx.graphics.getHeight();
+                    alButtons.get(8).fY = Gdx.graphics.getHeight();
+                    alButtons.get(9).fY = Gdx.graphics.getHeight();
+                    alButtons.get(10).fY = Gdx.graphics.getHeight();
+                    alButtons.get(11).fY = Gdx.graphics.getHeight();
+
                     game.updateScreen(2);
-                } else if (i == 1) {
+                } else if (i == 12) {
                     System.out.println("moves to main menu");
+                    //resets the y position of each level button
+                    alButtons.get(0).fY = Gdx.graphics.getHeight();
+                    alButtons.get(1).fY = Gdx.graphics.getHeight();
+                    alButtons.get(2).fY = Gdx.graphics.getHeight();
+                    alButtons.get(3).fY = Gdx.graphics.getHeight();
+                    alButtons.get(4).fY = Gdx.graphics.getHeight();
+                    alButtons.get(5).fY = Gdx.graphics.getHeight();
+                    alButtons.get(6).fY = Gdx.graphics.getHeight();
+                    alButtons.get(7).fY = Gdx.graphics.getHeight();
+                    alButtons.get(8).fY = Gdx.graphics.getHeight();
+                    alButtons.get(9).fY = Gdx.graphics.getHeight();
+                    alButtons.get(10).fY = Gdx.graphics.getHeight();
+                    alButtons.get(11).fY = Gdx.graphics.getHeight();
+
                     game.updateScreen(0);
                 }
                 PeetyTheBeefy.fMouseX = nSCREENWIDTH; // just moves mouse away from button
@@ -105,6 +145,18 @@ public class ScrStageSelect implements Screen {
         }
         if (fNewX >= 16) {
             fNewX -= 16;
+            alButtons.get(0).fY -= 4;
+            alButtons.get(1).fY -= 4;
+            alButtons.get(2).fY -= 4;
+            alButtons.get(3).fY -= 4;
+            alButtons.get(4).fY -= 8;
+            alButtons.get(5).fY -= 8;
+            alButtons.get(6).fY -= 8;
+            alButtons.get(7).fY -= 8;
+            alButtons.get(8).fY -= 12;
+            alButtons.get(9).fY -= 12;
+            alButtons.get(10).fY -= 12;
+            alButtons.get(11).fY -= 12;
         }
 
         batch.begin();
